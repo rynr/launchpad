dd76ec515b3ccc4b3a33d14a127bdc416c6e5eb7
Rainer Jung <rainer.jung@gmail.com>
2014-11-27 22:19:02 +0100

    Added a example implementation to the test folder

---
+++ src/test/java/org/rjung/utils/launchpad/example/BounceToLaunchpadExample.java
@@ -0,0 +1,47 @@
+package org.rjung.utils.launchpad.example;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.rjung.utils.launchpad.Launchpad;
+import org.rjung.utils.launchpad.LaunchpadHandler;
+import org.rjung.utils.launchpad.MidiCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BounceToLaunchpadExample implements Runnable {
+
+	// Change this to your setup
+	private static final String MIDI_DEVICE = "/dev/midi2";
+	private static final Logger LOG = LoggerFactory
+			.getLogger(BounceHandler.class);
+
+	public static void main(String[] args) throws FileNotFoundException {
+		new BounceToLaunchpadExample().run();
+	}
+
+	class BounceHandler implements LaunchpadHandler {
+		private Launchpad launchpad;
+
+		public BounceHandler(Launchpad launchpad) {
+			this.launchpad = launchpad;
+		}
+
+		public void recieve(MidiCommand command) {
+			try {
+				launchpad.send(command);
+			} catch (IOException e) {
+				LOG.error(e.getMessage(), e);
+			}
+		}
+	}
+
+	public void run() {
+		try {
+			Launchpad launchpad = new Launchpad(MIDI_DEVICE);
+			launchpad.addHandler(new BounceHandler(launchpad));
+		} catch (FileNotFoundException e) {
+			LOG.error(e.getMessage(), e);
+		}
+	}
+}
